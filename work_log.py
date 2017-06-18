5104b1cfcb4954f8c386a206834fb514ee9745fd
Ken LaRose <kmlarose2@gmail.com>
2017-06-18 09:01:14 -0500

    outline of project requirements

---
+++ work_log.py
@@ -0,0 +1,57 @@
+# PROJECT INSTRUCTIONS
+# ====================
+# Make sure your script runs without errors.
+# Catch exceptions and report errors to the user in a meaningful way.
+
+# As a user of the script, I should be prompted with a menu to choose
+# whether to add a new entry or lookup previous entries.
+
+# As a user of the script, if I choose to enter a new work log,
+# I should be able to provide a task name, a number of minutes spent working on it,
+# and any additional notes I want to record.
+
+# As a user of the script, if I choose to find a previous entry,
+# I should be presented with four options:
+# find by date
+# find by time spent
+# find by exact search
+# find by pattern
+# Note:
+# When finding by date, I should be presented with a list of dates with entries
+# and be able to choose one to see entries from.
+# When finding by time spent, I should be allowed to enter the number of minutes a task took
+# and be able to choose one to see entries from.
+# When finding by an exact string, I should be allowed to enter a string
+# and then be presented with entries containing that string in the task name or notes.
+# When finding by a pattern, I should be allowed to enter a regular expression
+# and then be presented with entries matching that pattern in their task name or notes.
+
+# When displaying the entries, the entries should be displayed in a readable format with
+# the date, task name, time spent, and notes information.
+
+# Make sure your coding style complies with PEP 8.
+
+# Before you submit your project for review,
+# make sure you can check off all of the items on the Student Project Submission Checklist.
+# The checklist is designed to help you make sure you’ve met the grading requirements
+# and that your project is complete and ready to be submitted!
+
+# EXTRA CREDIT
+# ============
+# Menu has a “quit” option to exit the program.
+# Entries can be deleted and edited, letting user change the date,
+# task name, time spent, and/or notes.
+# Entries can be searched for and found based on a date range.
+# For example between 01/01/2016 and 12/31/2016.
+# Entries are displayed one at a time with the ability to page through records
+# (previous/next/back).
+
+# NOTE:
+# To get an "Exceeds Expectations" grade for this project,
+# you'll need to complete each of the items in this section.
+# See the rubric in the "How You'll Be Graded" tab above for details on how you'll be graded.
+# If you’re shooting for the "Exceeds Expectations" grade,
+# it is recommended that you mention so in your submission notes.
+# Passing grades are final. If you try for the "Exceeds Expectations" grade,
+# but miss an item and receive a “Meets Expectations” grade, you won’t get a second chance.
+# Exceptions can be made for items that have been misgraded in review.
